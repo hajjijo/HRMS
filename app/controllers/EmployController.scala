382bdf2abf0a1e41e58228cd77d7667b101ed50f
k.jalali <hjjijo313@gmail.com>
2017-11-18 01:01:01 +0330

    edit employ is already complited

--- app/controllers/EmployController.scala
+++ app/controllers/EmployController.scala
@@ -11,7 +11,6 @@ class EmployController @Inject()(employService: EmployService)(implicit exec: Ex
   // http://localhost:9000/api/hrms/v1/employ/add
   //POST Json=> {"name":"nameTest","family":"familyTest","nationalId":"001945356","zipCode":"1025456","phone":"0912123567","address":"add-res-test","employStatus":"programmer","salary":10000}
   def addEmploy = Action.async(parse.json) { request => {
-    println(s">>>>>>>>>>>>>>>> this is your request ${request.body}")
     for {
       name <- (request.body \ "name").asOpt[String]
       family <- (request.body \ "family").asOpt[String]
@@ -33,10 +32,40 @@ class EmployController @Inject()(employService: EmployService)(implicit exec: Ex
         employStatus = employStatus,
         salary = salary
       )
-
       employService.addEmploy(employ).map(Ok(_))
-
     }
   }.getOrElse(Future {BadRequest(s"""{"ok":"false","message":"Wrong json format"}""")})
   }
+
+  // http://localhost:9000/api/hrms/v1/employ/:id<for example (2)>/edit
+  //PUT Json=> {"name":"nameTest2","family":"familyTest2","nationalId":"00194000","zipCode":"1025000","phone":"09120000","address":"add-res-test-2","employStatus":"CTO","salary":1000000}
+  def editEmploy(id: Long) = Action.async(parse.json) { request => {
+    for {
+      name <- (request.body \ "name").asOpt[String]
+      family <- (request.body \ "family").asOpt[String]
+      nationalId <- (request.body \ "nationalId").asOpt[String]
+      zipCode <- (request.body \ "zipCode").asOpt[String]
+      phone <- (request.body \ "phone").asOpt[String]
+      address <- (request.body \ "address").asOpt[String]
+      employStatus <- (request.body \ "employStatus").asOpt[String]
+      salary <- (request.body \ "salary").asOpt[Long]
+    } yield {
+
+      val employ = EmployEntity(
+        id = Some(id),
+        name = name,
+        family = family,
+        nationalId = nationalId,
+        zipCode = zipCode,
+        phone = phone,
+        address = address,
+        employStatus = employStatus,
+        salary = salary
+      )
+      employService.editEmploy(employ).map(Ok(_))
+    }
+  }.getOrElse(Future {BadRequest(s"""{"ok":"false","message":"Wrong json format"}""")})
+  }
+
+
 }
--- app/daos/EmployDao.scala
+++ app/daos/EmployDao.scala
@@ -1,24 +1,24 @@
 package daos
 
 import javax.inject.{Inject, Singleton}
-
 import models.EmployEntity
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
-
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class EmployDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit val ex: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
 
   import driver.api._
-
   val employTableQuery = TableQuery[UserTable]
 
   def insert(employEntity: EmployEntity): Future[Long] = {
     db.run(employTableQuery returning employTableQuery.map(_.id) += employEntity)
   }
 
+  def update(employEntity: EmployEntity): Future[Int] = {
+    db.run(employTableQuery.filter(_.id === employEntity.id.get).update(employEntity))
+  }
 
   @Singleton
   final class UserTable(tag: Tag) extends Table[EmployEntity](tag, "employs") {
